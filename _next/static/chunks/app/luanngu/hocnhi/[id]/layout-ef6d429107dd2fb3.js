cb0970b80eab1ff2721564858028411696433405
Duong_Pham_PC_fox <duongchung1999@gmail.com>
2025-08-11 10:29:27 +0700

    'release'

---
+++ _next/static/chunks/app/luanngu/hocnhi/[id]/layout-ef6d429107dd2fb3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7878,8244,238,604,7252,8200,2746,1294,961,5988,6569,3423,5451,7348,4581],{63521:function(){}},function(n){n.O(0,[2971,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
---
+++ _next/static/chunks/app/luanngu/hocnhi/edit/[id]/layout-bf90b7d3c5f25087.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[7348,8244,238,604,7252,8200,2746,1294,961,5988,6569,3423,5451,7878,4581],{63521:function(){}},function(n){n.O(0,[2971,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
