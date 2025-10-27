554c5f33f9a7ca185ef8df9ec19fa93fdcd83754
Duong_Pham_PC_fox <duongchung1999@gmail.com>
2025-10-27 11:33:55 +0700

    'release'

---
+++ _next/static/chunks/app/tuvung/chuyennganh/[id]/layout-3e77c13f0c2011b6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[9873,8244,604,238,9462,1294,8200,2746,7252,961,5988,846,6569,3423,5451,2092,7878,7348,4581],{63521:function(){}},function(n){n.O(0,[2971,7023,1744],function(){return n(n.s=63521)}),_N_E=n.O()}]);
